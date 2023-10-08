37c2fc8b0b03a2922055a0679d750421b749e993
Cheng Chi (@joychester) <joychester@gmail.com>
2023-10-07 22:14:45 -0700

    remove random pause

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use((req, res, next) => {
     // Fake pause
-    setTimeout(next, Math.floor( ( Math.random() * 1500 ) + 100 ) );
+    //setTimeout(next, Math.floor( ( Math.random() * 1500 ) + 100 ) );
+    next();
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
